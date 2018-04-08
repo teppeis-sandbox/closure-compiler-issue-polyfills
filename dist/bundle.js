7cb657cc2590284e9c212abbd9d45586a0132490
teppeis <teppeis@gmail.com>
2018-04-09 02:30:55 +0900

    add force-inject.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -2,6 +2,7 @@ alert(Symbol());
 alert("iterator" in Symbol);
 alert([][Symbol.iterator]());
 alert(typeof Number.isInteger);
+alert(typeof Number.isSafeInteger);
 alert(typeof Number.EPSILON);
 alert("function" === typeof String.prototype.repeat && "a".repeat(3));
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ alert(Symbol());
 alert('iterator' in Symbol);
 alert([][Symbol.iterator]());
 alert(typeof Number.isInteger);
+alert(typeof Number.isSafeInteger);
 alert(typeof Number.EPSILON);
 alert(typeof String.prototype.repeat === "function" && "a".repeat(3));
